3725e9b5fe10dde2d30e95d0d6b24b58d0352cc3
anasstasiia-martyniuk <anass.tttazja@gmail.com>
2025-05-05 13:29:37 +0200

    fixed photo, i hope

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -69,7 +69,7 @@ export const getContactsController = async (req, res) => {
 
   res.status(201).json({
     status: 201,
-    // message: `Successfully created a contact!`,
+    message: `Successfully created a contact!`,
     data,
   });
   };
